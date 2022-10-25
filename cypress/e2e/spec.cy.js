684ea9d4b40e338d881121c6e1418d2d7e0519d8
Saradhi <saradhi0001069@gmail.com>
2022-10-25 21:29:26 +0530

    Basic test-cases in cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,12 @@ describe('empty spec', () => {
     cy.visit('http://localhost:3000/requestquote')
   })
 
-  it("button should exist", function(){
-    cy.get(".btn").should("exist");
-    cy.get(".notThere").should("not.exist");
-});
+  it("product input field exist or not -- testing", function(){
+    cy.get("#product_details").should("exist");
+  });
+
+  it("Typing product in input field -- testing", function(){
+    cy.get("#product_details").type("Mobiles");
+  });
+
 })
