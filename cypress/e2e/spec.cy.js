6e20d9e68781411fe7194a4fc677c92fafac5a25
Saradhi <saradhi0001069@gmail.com>
2022-10-26 12:04:04 +0530

    spec test cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Request Form E2E Testing', () => {
+
+  it('Visit the site -- testing', () => {
     cy.visit('http://localhost:3000/requestquote')
   })
 
@@ -15,36 +16,49 @@ describe('empty spec', () => {
     cy.get("#location_from").type("Chennai").click();
   });
 
-  it("product input field exist or not -- testing", function(){
-    cy.get('[name="containers_quantity"]').type("13")
-  }); 
-
   it("Select Location to -- testing", function(){
     cy.get("#location_to").type("Liverpool").click();
   });
 
-  it("Associated services exist or not -- testing", function(){
-    cy.get("#link-checkbox").should("exist").click();
+  it("Select container quantity -- testing", function(){
+    cy.get('[name="containers_quantity"]').type("13")
+  }); 
+ 
+
+  it("Select date  -- testing", function(){
+    cy.get('[name="ready_to_load"]').type('2022-10-31');
   });
 
-  it("product input field exist or not -- testing", function(){
+  // it("Associated services exist or not -- testing", function(){
+  //   cy.get("#link-checkbox").should("exist").click();
+  // });
+
+  it("Fill additional information field -- testing", function(){
+    cy.get('[name="additional_information"]').type("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+  }); 
+
+  it("Add first name in contact info -- testing", function(){
     cy.get('[name="first_name"]').type("Jeff")
   }); 
 
-  it("product input field exist or not -- testing", function(){
+  it("Add last name in contact info -- testing", function(){
     cy.get('[name="last_name"]').type("Hardy")
   }); 
 
-  it("product input field exist or not -- testing", function(){
+  it("Add phone number in contact info -- testing", function(){
     cy.get('[name="phone"]').type("123456789")
   }); 
 
-  it("product input field exist or not -- testing", function(){
+  it("Add email in contact info -- testing", function(){
     cy.get('[name="email"]').type("jeff_hardy@outlook.com")
   }); 
 
-  it("Remember check box -- testing", function(){
+  it("Select T&C check box -- testing", function(){
     cy.get("#remember").click();
+  });
+
+  it("Form Submition -- testing", function(){
+    cy.get('[type="submit"]').click();
   });
 
 })
